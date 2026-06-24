6930688b635f25402265d7be38a1bf7563538ac1
Muhammad Muizzsuddin <muiz.idn@gmail.com>
2026-06-24 07:13:41 +0700

    Centralize html lang attribute into SITE_LANG constant

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
+export const SITE_LANG = 'id';
 export const SITE_TITLE = 'Balesaja.id | Jasa Setup Customer Service AI + Whitelabel';
 export const SITE_DESCRIPTION = 'Otomatisasi layanan pelanggan Anda dengan AI CS tercanggih. Dukungan whitelabel untuk agensi dan bisnis.';
